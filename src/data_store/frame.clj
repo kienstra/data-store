4dc51f06ce047a174934ae5f3c1636561723589a
Ryan Kienstra <kienstraryan@gmail.com>
2023-08-09 23:55:01 -0600

    Add integer and bulk string support

--- src/data_store/frame.clj
+++ src/data_store/frame.clj
@@ -1,15 +1,23 @@
-(ns data-store.frame)
+(ns data-store.frame
+  (:require [clojure.string :refer [index-of]]))
+
+(defn buffer->bytes [b i]
+  (if (Character/isDigit (get b i))
+     (recur b (+ i 1))
+    (Integer/parseInt (subs b 0 i))))
 
 (defn buffer->frame [b]
   (let [first-char (subs b 0 1)
         delim (.indexOf b "\r\n")]
-    (if
-     (= delim -1)
+    (cond
+      (or (= delim -1) (= b "$-1\r\n"))
       nil
-      (cond
-        (= first-char "+")
-        (subs b 1 delim)
-        (= first-char "-")
-        {:error (subs b 1 delim)}
-        (= first-char ":")
-        (Integer/parseInt (subs b 1 delim))))))
+      (= first-char "+")
+      (subs b 1 delim)
+      (= first-char "-")
+      {:error (subs b 1 delim)}
+      (= first-char ":")
+      (Integer/parseInt (subs b 1 delim))
+      (= first-char "$")
+      (let [bytes (buffer->bytes (subs b 1) 0)]
+        (subs b (+ 1 (count (str bytes))) (+ 2 bytes))))))
--- test/data_store/frame_test.clj
+++ test/data_store/frame_test.clj
@@ -1,9 +1,9 @@
 (ns data-store.frame-test
   (:require [clojure.test :refer [deftest is testing]]
-            [data-store.frame :refer [buffer->frame]]))
+            [data-store.frame :refer [buffer->bytes buffer->frame]]))
 
 (deftest frame-test
-  (testing "Simple String"
+  (testing "Simple string"
     (is (= nil (buffer->frame "+noend")))
     (is (= "OK" (buffer->frame "+OK\r\n")))
     (is (= "longer simple string" (buffer->frame "+longer simple string\r\n"))))
@@ -14,4 +14,10 @@
   (testing "Integer"
     (is (= nil (buffer->frame ":123")))
     (is (= 123 (buffer->frame ":123\r\n")))
-    (is (= 999999999 (buffer->frame ":999999999\r\n")))))
+    (is (= 999999999 (buffer->frame ":999999999\r\n"))))
+  (testing "Buffer to bytes"
+    (is (= 17 (buffer->bytes (subs "$17longerbulkstring\r\n" 1) 0))))
+  (testing "Bulk string"
+    (is (= nil (buffer->frame "$-1\r\n")))
+    (is (= "short" (buffer->frame "$5short\r\n")))
+    (is (= "longerbulkstring" (buffer->frame "$17longerbulkstring\r\n")))))
