850f719da03915e5279ea35c6f6b005a75f47808
Ryan Kienstra <kienstraryan@gmail.com>
2023-08-14 01:36:01 -0600

    Simplify writing

--- src/data_store/core.clj
+++ src/data_store/core.clj
@@ -2,37 +2,25 @@
   (:require [clojure.java.io :as io])
   (:import [java.net ServerSocket]))
 
-(defn receive
-  "Read a line of textual data from the given socket"
-  [reader]
-  (.readLine reader))
-
 (defn send-to-socket [writer msg]
-  (if
-   msg
-    (do
-      (.write writer "*1")
-      (.write writer msg))
-    nil))
+  (.write writer (str "*1" msg)))
 
 (defn serve [port handler]
-  (let [running (atom true)]
-    (future
-      (with-open [server-sock (ServerSocket. port)
-                  sock (.accept server-sock)
-                  reader (io/reader sock)
-                  writer (io/writer sock)]
+ (with-open
+       [server-sock (ServerSocket. port)
+        sock (.accept server-sock)
+        reader (io/reader sock)
+        writer (io/writer sock)]
         (while (.ready reader)
-          (let [msg-in (receive reader)
+          (let [msg-in (.readLine reader)
                 msg-out (handler msg-in)]
             (send-to-socket writer msg-out)))))
-    running))
 
 (defn handler [input]
   (println input)
   (if (= input "PING")
     "PONG"
-    nil))
+    ""))
 
 (defn -main []
   (serve 6379 handler))
