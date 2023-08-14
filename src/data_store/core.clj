d0cac74b26995f6304dd2ff0f98faf85624d5b94
Ryan Kienstra <kienstraryan@gmail.com>
2023-08-14 17:35:45 -0600

    Make PING work, but nothing else yet

--- src/data_store/core.clj
+++ src/data_store/core.clj
@@ -1,25 +1,21 @@
 (ns data-store.core
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [data-store.handler :refer [handler]])
   (:import [java.net ServerSocket]))
 
 (defn send-to-socket [writer msg]
   (.write writer msg))
 
-(defn serve [port handler]
- (with-open
-       [server-sock (ServerSocket. port)
-        sock (.accept server-sock)
-        reader (io/reader sock)
-        writer (io/writer sock)]
-        (while (.ready reader)
-          (let [msg-in (.readLine reader)
-                msg-out (handler msg-in)]
-            (send-to-socket writer msg-out)))))
-
-(defn handler [input]
-  (if (.contains input "PING")
-    "$4\r\nPONG\r\n"
-    ""))
+(defn serve [port]
+  (with-open
+   [server-sock (ServerSocket. port)
+    sock (.accept server-sock)
+    reader (io/reader sock)
+    writer (io/writer sock)]
+    (while (.ready reader)
+      (let [msg-in (.readLine reader)
+            msg-out (handler msg-in)]
+        (send-to-socket writer msg-out)))))
 
 (defn -main []
-  (serve 6379 handler))
+  (serve 6379))
--- src/data_store/handler.clj
+++ src/data_store/handler.clj
@@ -1,13 +1,7 @@
-(ns data-store.handler
-  (:require [clojure.string :refer [join split]]))
+(ns data-store.handler)
 
 (defn handler [input]
-  (let [spl (split input #" ")
-        command (nth spl 0)
-        args (rest spl)]
-    (cond
-      (= command "PING")
-      "PONG"
-      (= command "ECHO")
-      (join " " args)
-      :else nil)))
+  (if
+   (.contains input "PING")
+    "$4\r\nPONG\r\n"
+    ""))
