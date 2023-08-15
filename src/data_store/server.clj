12feed0d3578286e51e7399d1575a8d0b8145107
Ryan Kienstra <kienstraryan@gmail.com>
2023-08-15 16:57:33 -0600

    Remove future, as it's not needed

--- src/data_store/server.clj
+++ src/data_store/server.clj
@@ -6,16 +6,14 @@
 (defn stop [] (swap! running (fn [_] false)))
 
 (defn serve [handler port]
-  (future
-    (with-open [server-sock (ServerSocket. port)]
-      (while @running
-        (with-open [sock (.accept server-sock)
-                    reader (io/reader sock)
-                    writer (io/writer sock)]
-          (.write writer (handler
-                          (loop [acc []
-                                 r reader]
-                            (if (.ready r)
-                              (recur (conj acc (.readLine r)) r)
-                              acc))))))))
-  running)
+  (with-open [server-sock (ServerSocket. port)]
+    (while @running
+      (with-open [sock (.accept server-sock)
+                  reader (io/reader sock)
+                  writer (io/writer sock)]
+        (.write writer (handler
+                        (loop [acc []
+                               r reader]
+                          (if (.ready r)
+                            (recur (conj acc (.readLine r)) r)
+                            acc))))))))
