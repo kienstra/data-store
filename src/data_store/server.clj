362adb29306c01c5d49318b94d02834530831808
Ryan Kienstra <kienstraryan@gmail.com>
2023-08-15 21:59:04 -0600

    Handle state updates with immutable data

--- src/data_store/server.clj
+++ src/data_store/server.clj
@@ -8,12 +8,17 @@
 (defn serve [handler port]
   (with-open [server-sock (ServerSocket. port)]
     (while @running
-      (with-open [sock (.accept server-sock)
-                  reader (io/reader sock)
-                  writer (io/writer sock)]
-        (.write writer (handler
-                        (loop [acc []
-                               r reader]
-                          (if (.ready r)
-                            (recur (conj acc (.readLine r)) r)
-                            acc))))))))
+      (loop [state []]
+        (let [sock (.accept server-sock)
+              reader (io/reader sock)
+              writer (io/writer sock)]
+          (.write writer (handler
+                          (loop [acc []
+                                 r reader]
+                            (if (.ready r)
+                              (recur (conj acc (.readLine r)) r)
+                              acc))))
+          (.close writer)
+          (.close reader)
+          (println state)
+          (recur (conj state 1)))))))
