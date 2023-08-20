c390ad7da1a2f148def82e322cacf0cfaf6efd3d
Ryan Kienstra <kienstraryan@gmail.com>
2023-08-19 19:20:30 -0600

    Replace literal delimiters with delim

--- src/data_store/handler.clj
+++ src/data_store/handler.clj
@@ -7,36 +7,36 @@
   (let [command (nth input 2)]
     (cond
       (nil? command)
-      [store "-Error no command \r\n"]
+      [store (str "-Error no command" delim)]
       (= command "PING")
-      [store "$4\r\nPONG\r\n"]
+      [store (str "$4" delim "PONG" delim)]
       (= command "ECHO")
       (if (nth input 4 false)
         [store (str "+" (nth input 4) delim)]
-        [store "-Error nothing to echo\r\n"])
+        [store (str "-Error nothing to echo" delim)])
       (= command "SET")
       (cond
         (not (nth input 6 nil))
-        [store "-Error nothing to set\r\n"]
+        [store (str "-Error nothing to set" delim)]
         (not (string? (nth input 6 nil)))
-        [store "-Error not a string\r\n"]
+        [store (str "-Error not a string" delim)]
         :else (let [sub-command (nth input 7 nil)
                     sub-command-arg (nth input 8 nil)]
                 (if (and (= sub-command "EX") sub-command-arg)
-                  [(into store {(nth input 4) {:val (nth input 6) :exp (+ time (* 1000 sub-command-arg))}}) "+OK\r\n"]
-                  [(into store {(nth input 4) {:val (nth input 6)}}) "+OK\r\n"])))
+                  [(into store {(nth input 4) {:val (nth input 6) :exp (+ time (* 1000 sub-command-arg))}}) (str "+OK" delim)]
+                  [(into store {(nth input 4) {:val (nth input 6)}}) (str "+OK" delim)])))
       (= command "GET")
       (cond
         (not (nth input 4 nil))
-        [store "-Error nothing to get\r\n"]
+        [store (str "-Error nothing to get" delim)]
         (not (contains? store (nth input 4)))
-        [store "$-1\r\n"]
+        [store (str "$-1" delim)]
         (not (string? (:val (get store (nth input 4 nil)))))
-        [store "-Error not a string\r\n"]
+        [store (str "-Error not a string" delim)]
         :else (let [exp (:exp (get store (nth input 4)))
                     expired? (and exp (> time exp))]
                 (if
                  expired?
-                  [store "$-1\r\n"]
+                  [store (str "$-1" delim)]
                   [store (str "+" (:val (get store (nth input 4))) delim)])))
-      :else [store "-Error invalid command\r\n"])))
+      :else [store (str "-Error invalid command" delim)])))
