c96e25c6ef0e80238975782632f6c4b6dbc98fbb
Ryan Kienstra <kienstraryan@gmail.com>
2023-08-26 20:10:21 -0600

    Revert "Clean up extra methods, but there's an error"
    This reverts commit 749f2bd48d495a2039c537498e4cb592a044468b.

--- src/data_store/server.clj
+++ src/data_store/server.clj
@@ -2,29 +2,40 @@
 ; https://infi.nl/nieuws/writing-a-tcp-proxy-using-netty-and-clojure/
 (ns data-store.server
   (:import
-   [io.netty.bootstrap ServerBootstrap]
-   [io.netty.channel.socket.nio NioServerSocketChannel]
+   [io.netty.bootstrap ServerBootstrap Bootstrap]
+   [io.netty.channel.socket.nio NioServerSocketChannel NioSocketChannel]
    [io.netty.channel ChannelInboundHandlerAdapter
-    ChannelInitializer ChannelOption ChannelHandler ChannelFutureListener]
+    ChannelInitializer ChannelOption ChannelHandler ChannelFutureListener
+    ChannelOutboundHandlerAdapter]
    [io.netty.channel.nio NioEventLoopGroup]
+   [io.netty.handler.codec ByteToMessageDecoder]
+   [io.netty.handler.logging LoggingHandler LogLevel]
    [io.netty.buffer Unpooled]
-   [java.nio.charset StandardCharsets])
+   [java.nio ByteBuffer ByteOrder]
+   [java.io ByteArrayOutputStream]
+   [io.netty.handler.codec.bytes ByteArrayEncoder]
+   [java.util.concurrent LinkedBlockingQueue Executors]
+   [java.nio.charset StandardCharsets]
+   [java.util ArrayList]
+   [java.time Instant])
   (:require [clojure.string :refer [split]]
+            [clojure.pprint :refer [pprint]]
             [data-store.store :refer [store]]))
 
 (declare flush-and-close)
 
 (defn init-server-bootstrap
-  [group handlers]
+  [group handlers-factory]
   (.. (ServerBootstrap.)
       (group group)
       (channel NioServerSocketChannel)
       (childHandler
        (proxy [ChannelInitializer] []
          (initChannel [channel]
-                      (.. channel
-                          (pipeline)
-                          (addLast (into-array ChannelHandler handlers))))))
+           (let [handlers (handlers-factory)]
+             (.. channel
+                 (pipeline)
+                 (addLast (into-array ChannelHandler handlers)))))))
       (childOption ChannelOption/SO_KEEPALIVE true)
       (childOption ChannelOption/AUTO_READ false)
       (childOption ChannelOption/AUTO_CLOSE false)))
@@ -46,13 +57,17 @@
                                               (System/currentTimeMillis))]
                          (.writeAndFlush ctx (Unpooled/wrappedBuffer (.getBytes out)))
                          new-store))))
+      (channelInactive [ctx]
+        (when @outgoing-channel
+          (flush-and-close @outgoing-channel)))
       (exceptionCaught
        [ctx e]
-       ))))
+       (pprint e)
+       (.close (.. ctx channel))))))
 
-(defn start-server [port handlers]
+(defn start-server [port handlers-factory]
   (let [event-loop-group (NioEventLoopGroup.)
-        bootstrap (init-server-bootstrap event-loop-group handlers)
+        bootstrap (init-server-bootstrap event-loop-group handlers-factory)
         channel (.. bootstrap (bind port) (sync) (channel))]
 
     channel))
@@ -62,7 +77,16 @@
    (.writeAndFlush channel Unpooled/EMPTY_BUFFER)
    (.addListener ChannelFutureListener/CLOSE)))
 
+(defn print-netty-inbound-handler []
+  (proxy [ChannelInboundHandlerAdapter] []
+    (channelRead [ctx msg]
+      (pprint msg)
+      (.fireChannelRead ctx msg))))
+
 (defn serve! [port handler]
   (start-server
    port
-   [(server-handler handler)]))
+   (fn []
+     [(LoggingHandler. "proxy" LogLevel/INFO)
+      (print-netty-inbound-handler)
+      (server-handler handler)])))
